f4786ce730d3ad03cd96a053c25c8d07539752b6
Victor <innovimark@gmail.com>
2024-08-19 14:10:57 +0100

    update app.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@
 import { useEffect } from "react"
 
 export default function Redirect(){
-    // useEffect(()=>{
-    //     const redirectTo = (url: string) =>{
-    //         window.location.href = url
-    //     }
+    useEffect(()=>{
+        const redirectTo = (url: string) =>{
+            window.location.href = url
+        }
 
-    //     redirectTo('https://cxors.vercel.app/dashboard/home')
-    // })
+        redirectTo('https://cxors.vercel.app/dashboard/home')
+    })
     // return null
-    return <h1>This is page</h1>
+    return null
 }
