5982db270a551edfbee095ab1991b45a3a4bae64
Victor <innovimark@gmail.com>
2024-08-20 17:10:25 +0100

    add view count to router

--- src/app/[shortPath]/page.tsx
+++ src/app/[shortPath]/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 type UrlData = {
   original_url: string;
+  visit_count: number;
 };
 
 export default function LinkSlug({
@@ -11,7 +12,7 @@ export default function LinkSlug({
 }: {
   params: { shortPath: string };
 }) {
-  const [urlData, setUrlData] = useState<UrlData>();
+  const [urlData, setUrlData] = useState<UrlData | null>(null);
   const [error, setError] = useState<string | null>(null);
 
   const supabase = createClient();
@@ -21,31 +22,107 @@ export default function LinkSlug({
     window.location.href = url;
   };
 
-
   useEffect(() => {
     const fetchUrlDetails = async () => {
-      const { data, error } = await supabase
-        .from("urls")
-        .select("original_url")
-        .eq("short_path", shortPath)
-        .single();
+      try {
+        const { data, error } = await supabase
+          .from("urls")
+          .select("original_url, visit_count")
+          .eq("short_path", shortPath)
+          .single();
 
-      if (data) {
+        if (error || !data) {
+          setError("Redirect failed. URL not found or error occurred.");
+          return;
+        }
+
+        const { original_url, visit_count } = data;
         setUrlData(data);
-      } else {
-        setError(error.message);
-      }
 
-      redirectTo(`https://${urlData?.original_url}`)
+        const { error: updateError } = await supabase
+          .from("urls")
+          .update({ visit_count: visit_count + 1 })
+          .eq("short_path", shortPath);
+
+        if (updateError) {
+          setError("Internal Server Error");
+        } else {
+          redirectTo(`https://${original_url}`);
+        }
+      } catch (e) {
+        setError("An unexpected error occurred.");
+      }
     };
 
     fetchUrlDetails();
-  }, [supabase, shortPath, urlData?.original_url]);
-  // console.log(shortPath);
+  }, [shortPath, supabase]);
 
   return (
     <h1>
-      redirecting...
+      {error ? error : "Redirecting..."}
     </h1>
   );
 }
+
+// "use client";
+
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
+
+// interface ShortUrlPageProps {
+//   params: {
+//     shortPath: string;
+//   };
+// }
+
+// const ShortUrlPage: React.FC<ShortUrlPageProps> = ({ params }) => {
+//   const { shortPath } = params;
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     if (shortPath) {
+//       console.log('me')
+//       fetch(`../api/track-visit?shortPath=${shortPath}`, {
+//         method: "GET",
+//         headers: {
+//           "Content-Type": "application/json",
+//           "Origin": window.location.origin,
+//         },
+//       })
+//       .then((response) => {
+//         if (response.redirected) {
+//       // .then((response) => response.json())
+//       // .then((data) => {
+//       //   if (data.original_url) {
+//       //     const validUrl = data.original_url.startsWith('http://') || data.original_url.startsWith('https://')
+//       //       ? data.original_url
+//       //       : `https://${data.original_url}`;
+            
+//           // Redirect after tracking is done
+//           window.location.href = response.url;;
+//         }
+//       })
+//       .catch(error => console.error('Error tracking visit:', error));
+//     }
+//   }, [shortPath]);
+  //       .then((response) => {
+  //         if (response.ok && response.redirected) {
+  //           console.log("Redirecting to:", response.url);
+  //           router.push(response.url);
+  //         } else {
+  //           return response.json().then((errorData) => {
+  //             console.error(
+  //               "Tracking failed:",
+  //               errorData.error || response.statusText
+  //             );
+  //           });
+  //         }
+  //       })
+  //       .catch((error) => console.error("Error tracking visit:", error));
+  //   }
+  // }, [shortPath, router]);
+
+//   return <div>Redirecting...</div>;
+// };
+
+// export default ShortUrlPage;
