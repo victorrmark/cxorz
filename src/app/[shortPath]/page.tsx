85ce788c66843c7ac96092981673586abca112e5
Victor <innovimark@gmail.com>
2024-08-21 00:00:36 +0100

    added location and country

--- src/app/[shortPath]/page.tsx
+++ src/app/[shortPath]/page.tsx
@@ -58,7 +58,7 @@ export default function LinkSlug({
           .from("urls")
           .update({ 
             visit_count: visit_count + 1,
-            last_location: location.city
+            last_location: `${location.city}, ${location.country_name}`
           })
           .eq("short_path", shortPath);
 
@@ -82,65 +82,3 @@ export default function LinkSlug({
   );
 }
 
-// "use client";
-
-// import { useEffect } from "react";
-// import { useRouter } from "next/navigation";
-
-// interface ShortUrlPageProps {
-//   params: {
-//     shortPath: string;
-//   };
-// }
-
-// const ShortUrlPage: React.FC<ShortUrlPageProps> = ({ params }) => {
-//   const { shortPath } = params;
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     if (shortPath) {
-//       console.log('me')
-//       fetch(`../api/track-visit?shortPath=${shortPath}`, {
-//         method: "GET",
-//         headers: {
-//           "Content-Type": "application/json",
-//           "Origin": window.location.origin,
-//         },
-//       })
-//       .then((response) => {
-//         if (response.redirected) {
-//       // .then((response) => response.json())
-//       // .then((data) => {
-//       //   if (data.original_url) {
-//       //     const validUrl = data.original_url.startsWith('http://') || data.original_url.startsWith('https://')
-//       //       ? data.original_url
-//       //       : `https://${data.original_url}`;
-            
-//           // Redirect after tracking is done
-//           window.location.href = response.url;;
-//         }
-//       })
-//       .catch(error => console.error('Error tracking visit:', error));
-//     }
-//   }, [shortPath]);
-  //       .then((response) => {
-  //         if (response.ok && response.redirected) {
-  //           console.log("Redirecting to:", response.url);
-  //           router.push(response.url);
-  //         } else {
-  //           return response.json().then((errorData) => {
-  //             console.error(
-  //               "Tracking failed:",
-  //               errorData.error || response.statusText
-  //             );
-  //           });
-  //         }
-  //       })
-  //       .catch((error) => console.error("Error tracking visit:", error));
-  //   }
-  // }, [shortPath, router]);
-
-//   return <div>Redirecting...</div>;
-// };
-
-// export default ShortUrlPage;
